71d1cd20ff420d274cc80f59e79cab54beb2973b
Sameer Akhtar <sameer.akhtar.52.sa@gmail.com>
2025-12-09 11:23:06 +0530

    login page as per design

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,15 +1,40 @@
 
-import React from "react"
-import LoginForm from "@/components/form/login"
 
+import LoginForm from "@/components/form/login"
+import { Button } from "@/components/ui/button";
+import LogoBanner from "../../../components/LogoBanner";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page(){
     return(
         <>
-            <div className="w-full h-screen flex items-center justify-center bg-white">
-                <LoginForm/>
-                
-            </div>
+           <div className="w-full h-max md:h-screen md:flex-col md:flex">
+                 <div className=" w-full h-max flex items-center justify-between px-2 border-b py-4 sm:px-8 md:px-12 lg:px-16">
+                    <LogoBanner/>
+                    <Button className="bg-brand-blue text-xs">
+                        <Link href="/auth/signup">Create an account</Link>
+                    </Button>
+                </div>
+                <div className=" w-full h-max flex flex-col gap-4 items-center justify-center bg-white mt-4 md:h-full md:flex-row">
+                    <div className="w-full h-max px-1 md:w-1/2 md:h-full grid place-items-center ">
+                        <LoginForm/>
+                    </div>
+                    <div className="w-full h-max flex items-center justify-center p-2 md:w-1/2 md:h-full md:justify-start">
+                        <div className="relative w-full max-w-lg aspect-square rounded-md overflow-hidden shadow-sm">
+                            <Image src={'/Images/college-students-different-ethnicities-cramming 1.png'} alt="college-students-different-ethnicities-cramming 1" width={400} height={400} className="w-full h-full object-cover" />
+                            <div className="absolute bottom-0 w-full h-1/2 flex flex-col px-4 py-8 bg-linear-to-t from-0% from-black/90 to-100% to-black/10 text-white">
+                            <div className="w-full h-fit flex flex-col mt-auto md:gap-2">
+                                    <span className="font-bold text-xl">Great Mentorship</span>
+                                <span className="text-sm">Get help from seasoned mentors to help navigate your ivy league journey.</span>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+           </div>
         </>
     )
 }
--- components/form/login.tsx
+++ components/form/login.tsx
@@ -11,6 +11,7 @@ import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 import Link from "next/link";
 import { toast } from "sonner";
+import { Eye } from "lucide-react";
 
 const formSchema = z.object({
     email : z.email({error:'invalid email'}),
@@ -65,9 +66,9 @@ export default function LoginForm(){
 
     return(
         <>
-            <Card className="w-full max-w-md h-screen py-10 sm:h-max">
+            <Card className="w-full max-w-md h-max py-10 bg-[#070750]/2 sm:h-max">
                 <CardHeader>
-                    <CardTitle>Login</CardTitle>
+                    <CardTitle>Welcome back</CardTitle>
                     <CardDescription>
                         Login into your account to access resources
                     </CardDescription>
@@ -85,7 +86,7 @@ export default function LoginForm(){
                                             {...field}
                                             id="login-form-email"
                                             aria-invalid={fieldState.invalid}
-                                            placeholder="janedoe@example.com"
+                                            placeholder="enter your email address"
                                             autoComplete="off"
                                             className="border-neutral-400"
                                         />
@@ -97,32 +98,54 @@ export default function LoginForm(){
                                 name="password"
                                 control={form.control}
                                 render={({field, fieldState})=>(
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                                        <Input 
+                                    <Field data-invalid={fieldState.invalid} className="relative">
+                                        <FieldLabel htmlFor={field.name} >Password</FieldLabel>
+                                        <div className="w-full h-fit flex">
+                                            <Input 
                                             {...field}
                                             id="login-form-password"
                                             aria-invalid={fieldState.invalid}
-                                            placeholder="password here"
+                                            placeholder="enter your password"
                                             autoComplete="off"
-                                            className="border-neutral-400"
+                                            className="border-neutral-400 flex-1"
+                                            type='password'
                                         />
+                                        {/* <Activity mode="visible">
+                                            <Button variant={'ghost'} className="w-fit absolute right-0 hover:bg-transparent"><Eye/></Button>
+                                        </Activity> */}
+                                        </div>
                                         {fieldState.invalid && ( <FieldError errors={[fieldState.error]}/>)}
                                     </Field>
                                 )}
                             />
                             <div className="w-full h-fit">
                                 <Activity mode={!showFormSubmitBtn ? 'hidden' : 'visible'}>
-                                    <Button className="w-full" type="submit" form="login-form" disabled={submitDisable}>Submit</Button>
+                                    <Button className="w-full bg-brand-blue" type="submit" form="login-form" disabled={submitDisable}>Login to your account</Button>
                                 </Activity>
                                 <Activity mode={showFormSubmitBtn ? 'hidden' : 'visible'}>
-                                    <Button type="button" className="w-full"><Spinner/></Button>
+                                    <Button type="button" className="w-full bg-brand-blue"><Spinner/></Button>
                                 </Activity>
                             </div>
                         </FieldGroup>
                     </form>
                 </CardContent>
-                <CardFooter>
+                <CardFooter className="flex-col">
+                    <div className="w-full h-fit flex flex-col items-center px-4 pb-8 gap-2">
+                        <div className="w-full flex flex-row items-center gap-3">
+                            <div className="flex-1 bg-slate-300 h-px"></div>
+                            <div>or</div>
+                            <div className="flex-1 bg-slate-300 h-px"></div>
+
+                        </div>
+                        <Button variant={'outline'} className="w-full flex items-center justify-center">
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48">
+                                <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                                </svg>
+                            </span>
+                            <span>continue with Google</span>
+                        </Button>
+                    </div>
                     <div className="w-full h-fit flex items-center justify-center gap-1 text-sm">
                        New to Global Dream Connect? <Link className="text-blue-500 underline" href={'/auth/signup'}>Sign up</Link>
                     </div>
